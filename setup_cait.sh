c92f9cd96f45e4ec8b1d65ad68c461beb3db28eb
Michael Ng <michaelng@cortic.ca>
2020-10-07 13:46:47 -0700

    Polishing setup scripts

--- setup_cait.sh
+++ setup_cait.sh
@@ -15,11 +15,11 @@ sudo touch /etc/docker/daemon.json
 sudo bash -c 'echo "{\"experimental\": true}" > /etc/docker/daemon.json'
 
 sudo systemctl restart docker
-sudo docker pull registry.cortic.ca/cait-stt_tts
-sudo docker pull registry.cortic.ca/cait-nlp
-sudo docker pull registry.cortic.ca/cait-vision
-sudo docker pull registry.cortic.ca/cait-control
-sudo docker pull registry.cortic.ca/cait-broker
+sudo docker pull cortictech/speech:0.51
+sudo docker pull cortictech/nlp:0.51
+sudo docker pull cortictech/vision:0.51
+sudo docker pull cortictech/control:0.51
+sudo docker pull cortictech/broker:0.51
 sudo apt-get install v4l-utils -y
 sudo apt-get install portaudio19-dev mplayer -y
 sudo pip3 install docker-compose flask Flask-HTTPAuth flask_cors paho-mqtt gunicorn pyaudio
